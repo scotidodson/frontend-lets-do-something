f5b771ee2ba6fa7ce135cc0976b7f7d0e160de40
scotidodson <scotidodson@gmail.com>
2019-02-16 20:06:36 -0500

    changed home styles to scss

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,19 +4,17 @@ import { Link } from 'react-router-dom'
 class Home extends Component {
   render() {
     return (
-      <div className="home-container">
-        <div className="home">
+      <div className="home">
           <Link to="/brainstorm">
-            <div className="home-brainstorm">
+            <div className="launch brainstorm">
               <h3>Brainstorm</h3>
             </div>
           </Link>
           <Link to="/host">
-            <div className="home-host">
+            <div className="launch host">
               <h3>Host</h3>
             </div>
           </Link>
-        </div>
       </div>
     );
   }

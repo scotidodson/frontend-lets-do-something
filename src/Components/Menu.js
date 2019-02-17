69f0f6a69f1a0f2932b037fedeb5184c2fbb4494
scotidodson <scotidodson@gmail.com>
2019-02-17 00:54:48 -0500

    updated menu

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
 class Menu extends Component {
+  handleBack = () => {
+    this.props.history.goBack()
+  }
+
   render() {
     return (
       <div className="menu-page">
 
+        <div className="back">
+          <h3 onClick={this.handleBack}>&times;</h3>
+        </div>
         <div className="menu-div">
           <h3><Link to="/events">EVENTS</Link></h3>
         </div>
